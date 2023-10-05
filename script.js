f7be66096bfc7379e6041d2acd1727f96a144dc9
Thiago Santos <BAREGAMEPLAY14@GMAIL.COM>
2023-10-04 21:29:18 -0300

    Funcionalidades da calculadora

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ function inserir(valor) {
     display.classList = 'style-display';
 };
 
+//Function para limpar o display
 function limpar() {
     display.innerHTML = ' ';
 };
 
+//Function para fazer o calculo desejado ao clicar no botão igual
 function btnigual() {
     if (display.textContent != undefined) {
         display.innerHTML = eval(display.innerHTML);
